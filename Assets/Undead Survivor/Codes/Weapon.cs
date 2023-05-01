d51a098842b6ebe59b3805b9cf18d177ccabb644
SKINIKE <83528236+SKINIKE@users.noreply.github.com>
2023-05-01 22:25:21 +0900

    총알 발사 및 몹 사망 구현완료 ui구현중

--- Assets/Undead Survivor/Codes/Weapon.cs
+++ Assets/Undead Survivor/Codes/Weapon.cs
@@ -9,11 +9,19 @@ public class Weapon : MonoBehaviour
     public int PrefabId;
     public float damage;
     public int count;
-    public int speed;
+    public float speed;
+
+    float timer;
+    Player player;
+
+    void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
 
     void Start()
     {
-        init();
+        Init();
     }
 
     void Update()
@@ -24,6 +32,14 @@ public class Weapon : MonoBehaviour
                 transform.Rotate(Vector3.back * speed * Time.deltaTime);
                 break;
             default:
+                timer += Time.deltaTime;
+
+                if(timer > speed)
+                {
+                    timer = 0f;
+                    Fier();
+                }
+
                 break;
         }
 
@@ -44,17 +60,17 @@ public class Weapon : MonoBehaviour
         }
     }
 
-    public void init()
+    public void Init()
     {
         switch (id)
         {
             case 0:
-                speed = -150;
+                speed = 150;
                 Batch();
                 break;
 
             default:
-
+                speed = 0.3f;
                 break;
         }
     }
@@ -81,7 +97,22 @@ public class Weapon : MonoBehaviour
             Vector3 rotVec = Vector3.forward * 360 * i / count;
             bullet.Rotate(rotVec);
             bullet.Translate(bullet.up * 1.5f, Space.World);
-            bullet.GetComponent<Bullet>().init(damage, -1); // -1 is Infinity Per.
+            bullet.GetComponent<Bullet>().Init(damage, -1, Vector3.zero); // -1 is Infinity Per.
         }
+    }
+
+    void Fier()
+    {
+        if(player.scanner.nearestTarget == null) { return; }
+
+        Vector3 targetPos = player.scanner.nearestTarget.position;
+        Vector3 dir = targetPos - transform.position;
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.instance.pool.Get(PrefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+
+        bullet.GetComponent<Bullet>().Init(damage, count, dir);
     }
 }
