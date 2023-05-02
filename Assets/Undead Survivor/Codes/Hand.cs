e1a762d60a2867f9db494ccf5ab74c71549a9dc4
SKINIKE <83528236+SKINIKE@users.noreply.github.com>
2023-05-02 23:41:50 +0900

    양손 무기 구현

--- Assets/Undead Survivor/Codes/Hand.cs
+++ Assets/Undead Survivor/Codes/Hand.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isLeft;
+    public SpriteRenderer spriter;
+
+    SpriteRenderer player;
+
+    Vector3 rightPos = new Vector3(0.35f, -0.15f, 0f);
+    Vector3 rightPosReverse = new Vector3(-0.15f, -0.15f, 0f);
+    Quaternion leftRot = Quaternion.Euler(0f, 0f, -35f);
+    Quaternion leftRotReverse = Quaternion.Euler(0f, 0f, -135f);
+
+
+    void Awake()
     {
-        
+        player = GetComponentsInParent<SpriteRenderer>()[1];
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        bool isReverse = player.flipX;
+
+        if (isLeft) //근접무기
+        {
+            transform.localRotation = isReverse ? leftRotReverse : leftRot;
+            spriter.flipY = isReverse;
+            spriter.sortingOrder = isReverse ? 4 : 6;
+        }
+        else //원거리무기
+        {
+            transform.localPosition = isReverse ? rightPosReverse : rightPos;
+            spriter.flipX = isReverse;
+            spriter.sortingOrder = isReverse ? 6 : 4;
+        }
     }
 }
