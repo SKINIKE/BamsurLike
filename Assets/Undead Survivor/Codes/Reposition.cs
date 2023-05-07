92b23b575dad0edb1c100493ce043a1e8b6b6fc3
SKINIKE <83528236+SKINIKE@users.noreply.github.com>
2023-05-07 23:47:31 +0900

    로직개선

--- Assets/Undead Survivor/Codes/Reposition.cs
+++ Assets/Undead Survivor/Codes/Reposition.cs
@@ -17,24 +17,23 @@ public class Reposition : MonoBehaviour
 
         Vector3 playerPos = GameManager.instance.player.transform.position;
         Vector3 myPos = transform.position;
-        Vector3 playerDir = GameManager.instance.player.inputVec;
-
-        float dirX = playerPos.x - myPos.x;
-        float dirY = playerPos.y - myPos.y;
-        float diffX = Mathf.Abs(dirX);
-        float diffY = Mathf.Abs(dirY);
-
-        dirX = dirX > 0 ? 1 : -1;
-        dirY = dirY > 0 ? 1 : -1;
 
         switch (transform.tag)
         {
             case "Ground":
-                if(diffX > diffY)
+                float diffX = playerPos.x - myPos.x;
+                float diffY = playerPos.y - myPos.y;
+                float dirX = diffX > 0 ? 1 : -1;
+                float dirY = diffY > 0 ? 1 : -1;
+
+                diffX = Mathf.Abs(diffX);
+                diffY = Mathf.Abs(diffY);
+
+                if (diffX > diffY)
                 {
                     transform.Translate(Vector3.right * dirX * 40);
                 }
-                else if (diffY > diffX)
+                else if (diffX < diffY)
                 {
                     transform.Translate(Vector3.up * dirY * 40);
                 }
@@ -42,7 +41,9 @@ public class Reposition : MonoBehaviour
             case "Enemy":
                 if (coll.enabled)
                 {
-                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
+                    Vector3 dist = playerPos - myPos;
+                    Vector3 ran = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+                    transform.Translate(ran + dist * 2);
                 }
                 break;
         }
